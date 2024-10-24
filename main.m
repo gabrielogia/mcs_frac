aac248d0b29c89457b670d0f8fa87fb50f1c705b
Gabriel Duarte <dacos033@umn.edu>
2024-10-24 11:06:18 -0500

    adding survival probability file

--- main.m
+++ main.m
@@ -3,17 +3,18 @@ clc
 clear
 close all
 
+%%
 ndof = 1;
 rng(42);
 
-barrier = 0.35;
+barrier = 0.02;
 is_base = true;
 m = 1*ones(1,ndof); %mass vector
-damping = 1*ones(1,ndof); %damping vector
-stiffness = 30*ones(1,ndof); %stiffness vector
+damping = 5*ones(1,ndof); %damping vector
+stiffness = 100*ones(1,ndof); %stiffness vector
 ex = 0.5*ones(1,ndof);
 ev = 0*ones(1,ndof);
-T = 30;
+T = 2;
 b0 = 0.15;
 S0 = 1;
 alpha = 0.75;
@@ -21,25 +22,22 @@ alpha = 0.75;
 [M, C, K] = get_mck(m, damping, stiffness, ndof);
 
 %% plot ps
+% Maximum frequency of the excitation process.
+fmax_ps = 50; 
+
 vec_time = linspace(0, T, 40);
-vec_freq = linspace(0, 50, 41);
+vec_freq = linspace(0, fmax_ps, 41);
 
 for i=1:numel(vec_time)
-
     for j=1:numel(vec_freq)
-
         f = vec_freq(j);
         t = vec_time(i);
         psec(i,j) = evolutionary_power_spectrum(f, t, S0, b0);
-
     end
 end
 
 % figure
 % surf(vec_time,vec_freq,psec')
-
-% Maximum frequency of the excitation process.
-fmax_ps = 50; 
 
 %% statistical_linearization
 disp("sl")
@@ -81,7 +79,7 @@ end
 
 %% mcs
 disp('mcs')
-ns = 10;
+ns = 16;
 tic
 [var_x, time_out, first_passage_time] = displacement_variance_mcs_mdof(ex, alpha, S0, b0, time, ns, M, C, K, stiffness, ndof, barrier, is_base, fmax_ps);
 toc
@@ -130,71 +128,7 @@ ylabel('Damping equivalent')
 
 %% survival probability
 disp("sp")
-
-r_2 = zeros(numel(t)-1, 1);
-time_domain = linspace(time(1),time(end),300);
-%time_domain(1) = time(1);
-Pb = zeros(ndof, numel(time_domain));
-
-for dof=1:ndof
-    c_dof = c(dof,:);
-    beta_eq_dof = beta_eq(dof,:);
-    omega_eq_2_dof = omega_eq_2(dof,:);
-    beta_eq_dof(1) = beta_eq_dof(2);
-    omega_eq_2_dof(1) = omega_eq_2_dof(2);
-
-    %for i = 2:numel(time_domain)
-    %    time_domain(i) = time_domain(i-1) + q*2*pi/sqrt((omega_eq_2_dof(i-1)));
-    %end
-
-    c_new = interp1(t, c_dof, time_domain, 'pchip');
-    beta_eq_new = interp1(t, beta_eq_dof, time_domain, 'pchip');
-    omega_eq_2_new = interp1(t, omega_eq_2_dof, time_domain, 'pchip');
-    r_2(1) = 0;
-    
-    for i = 2:numel(time_domain)
-        tau = time_domain(i) - time_domain(i-1);
-        r_2(i) = (c_new(i-1)/c_new(i))*(1 - (beta_eq_new(i-1))*tau);
-    end
-    
-    N = 5;
-    k = 1:1:N;
-    
-    for i = 2:numel(r_2)
-        A = (-barrier^2)/(2*c_new(i)*(1-r_2(i)));
-        B = (-barrier^2)/(2*c_new(i-1)*(1-r_2(i)));
-        D0 = (1-r_2(i))*exp(A)*(1 - exp(B));
-    
-        soma = 0;
-        for n=1:N
-            A = (r_2(i)^n)*((2*n+2));
-            B = (c_new(i-1)*c_new(i))^(n+1);
-            C = ((1 - r_2(i))^(2*n+1))*prod((2*(1:n)).^2);
-            upper_incomplete_gamma_i = igamma(n+1,(barrier^2)/(2*c_new(i)*(1-r_2(i))));
-            upper_incomplete_gamma_i_minus = igamma(n+1,(barrier^2)/(2*c_new(i-1)*(1-r_2(i))));
-            complete_gamma = gamma(n+1);
-            Ln = ((4^n)*((1 - r_2(i))^(2*n+2))*(c_new(i-1)^(n+1))*(c_new(i)^(n+1))*upper_incomplete_gamma_i)*(complete_gamma - upper_incomplete_gamma_i_minus);
-            Dn = (A/(B*C))*Ln;
-            soma = soma + Dn;
-        end
-        Q(i,1) = D0 + soma;
-        H(i,1) = 1 - exp((-barrier^2)/(2*c_new(i-1)));
-        F(i,1) = Q(i,1)/H(i,1);
-    end
-    
-    Pb_dof = zeros(1, ndof);
-    for i = 1:numel(time_domain)
-        aux = 1;
-        for p = 1:i
-            if (isnan(F(p)))
-                F(p) = 0;
-            end
-            aux = aux*(1 - F(p));
-        end
-        Pb_dof(1, i) = aux;
-    end
-    Pb(dof, :) = Pb_dof;
-end
+[Pb, time_domain] = survival_probability(ndof, barrier, c, beta_eq, omega_eq_2, time, t);
 
 %% plot pb
 fig = figure(5);
@@ -208,7 +142,7 @@ for i=1:ndof
     title('First passage time')
     xlabel('Time')
     ylabel('Survivor Propability')
-    xlim([0, 30])
+    xlim([0, T])
 end
 
 str = sprintf('plots/first_passage_time_pdf_ndof_%d_alpha_%.2f_barrier_%.2f_mcs_%d.pdf', ndof, alpha, barrier, ns);
