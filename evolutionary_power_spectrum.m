5eac255fa89cdfc5b0ec3246aaa09d1672425539
Gabriel Duarte <dacos033@umn.edu>
2024-12-18 19:55:54 -0600

    changing power spectrum

--- evolutionary_power_spectrum.m
+++ evolutionary_power_spectrum.m
@@ -5,7 +5,7 @@ function Sw = evolutionary_power_spectrum(freq, t)
 
     aux1 = (freq./(5*pi)).^2;
     aux2 = (freq./(10*pi)).^2;
-    %Sw = S0.*aux1.*(exp(-b0.*t).*t.^2).*exp(-aux2.*t);
+    Sw = S0.*aux1.*(exp(-b0.*t).*t.^2).*exp(-aux2.*t);
 
     % White noise:
-    Sw = S0;
+    %Sw = S0;
