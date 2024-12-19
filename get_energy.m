dda738f86c9c72c3b6838e352dcca02a1f1c656b
Gabriel Duarte <dacos033@umn.edu>
2024-12-18 21:47:35 -0600

    creating linspace around a center point

--- get_energy.m
+++ get_energy.m
@@ -1,10 +1,17 @@
-function [sfun,weq2,beq] = get_energy(s2t,t,weq2max,betamax,q,nw,nb)
-    weq2 = linspace(0.1,weq2max,nw);
-    beq = linspace(0.1,betamax,nb);
+function [sfun,weq2,beq] = get_energy(s2t,t,weq2_center,beta_center,q,nw,nb)
+    init = 0.01;
 
-    for i=1:nw
+    L = init  + rem(weq2_center - init, (weq2_center - init)/nw);
+    H = weq2_center*2 - rem(weq2_center*2 - weq2_center, (weq2_center*2 - weq2_center)/nw);
+    weq2 = L:(weq2_center - init)/nw:H;
+
+    L = init  + rem(beta_center - init, (beta_center - init)/nb);
+    H = beta_center*2 - rem(beta_center*2 - beta_center, (beta_center*2 - beta_center)/nb);
+    beq = L:(beta_center - init)/nb:H;
+
+    for i=1:numel(weq2)
         w2 = weq2(i);
-        parfor j=1:nb
+        parfor j=1:numel(beq)
             Sw = @(x)( evolutionary_power_spectrum(x, t) );
             sfun(i,j) = (s2t - 2*integral(@(x)Sw(x)./( abs(w2 - x.^2 + beq(j)*(1i*x).^q).^2 ),0,Inf)).^2;
         end
