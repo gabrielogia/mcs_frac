fce06882a1b01e64cd564c9b35caa640df9f25ff
Gabriel Duarte <dacos033@umn.edu>
2024-12-18 19:56:00 -0600

    changing energy

--- get_energy.m
+++ get_energy.m
@@ -1,14 +1,12 @@
-function [energy] = get_energy(varx_sl, omega_eq_2, aux, ndof, time, q)
-    for i = 1:numel(ndof)
-        for j = 1:numel(omega_eq_2)
-            t = time(j);
-            sig2t = varx_sl(i,j);
-            w2 = omega_eq_2(i,j);
-            parfor k = 1:numel(aux)
-                Sw = @(x)( evolutionary_power_spectrum(x, t) );
-                sfun = (sig2t - 2*integral(@(x)Sw(x)./( abs(w2 - x.^2 + aux(k)*(1i*x).^q).^2 ),0,Inf)).^2;
-                energy(j,k) = sfun;
-            end
+function [sfun,weq2,beq] = get_energy(s2t,t,weq2max,betamax,q,nw,nb)
+    weq2 = linspace(0.1,weq2max,nw);
+    beq = linspace(0.1,betamax,nb);
+
+    for i=1:nw
+        w2 = weq2(i);
+        parfor j=1:nb
+            Sw = @(x)( evolutionary_power_spectrum(x, t) );
+            sfun(i,j) = (s2t - 2*integral(@(x)Sw(x)./( abs(w2 - x.^2 + beq(j)*(1i*x).^q).^2 ),0,Inf)).^2;
         end
     end
 end
