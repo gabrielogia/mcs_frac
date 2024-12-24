92a32ed04add4fbe1255f8ea9f076246ae030703
Gabriel Duarte <dacos033@umn.edu>
2024-12-24 15:37:18 -0600

    new plots

--- plot_opt_surface.m
+++ plot_opt_surface.m
@@ -3,6 +3,15 @@ clc
 clear
 close all
 
+list_factory = fieldnames(get(groot,'factory'));
+index_interpreter = find(contains(list_factory,'Interpreter'));
+for i = 1:length(index_interpreter)
+    default_name = strrep(list_factory{index_interpreter(i)},'factory','default');
+    set(groot, default_name,'latex');
+end
+
+set(groot,'defaultAxesFontSize',12)
+
 %% Structure data 
 
 % For reproducibility:
@@ -88,10 +97,10 @@ load(strcat('data/displacement_variance_', str, '.mat'))
 load(strcat('data/betaeq_', str, '.mat'))
 load(strcat('data/omegaeq_', str, '.mat'))
 
-%% compute energy diff
+%% compute surface values
 for i=1:ndof
     [sfun_values,wq2,bq] = get_integral_values_mesh(varx_sl(i,end), time(end), w2(i,end), ...
-        beta_original(i,end), q, 51, 51, S0);
+        beta_original(i,end), q, 101, 101, S0);
 
     idx_w2 = find(round(wq2,5)==round(w2(i,end),5));
     idx_beta = find(round(bq,5)==round(beta_original(i,end),5));
@@ -101,33 +110,63 @@ for i=1:ndof
     data(i).bq = bq;
     data(i).idx_w2 = idx_w2;
     data(i).idx_beta = idx_beta;
-    
+end    
+
+%% plot
+letters = ['a', 'b', 'c'];
+for i=1:1
+    sfun_values = data(i).sfun_values;
+    wq2 = data(i).wq2;
+    bq = data(i).bq;
+    idx_w2 = data(i).idx_w2;
+    idx_beta = data(i).idx_beta;
+
     fig = figure(1);
-    subplot(1,ndof,i)
+    subplot(1,1,i)
     xlim([0 max(wq2)])
     ylim([0 max(bq)])
+    zlim([log(min(min(sfun_values))) log(max(max(sfun_values)))])
+
     scatter3(wq2(idx_w2), bq(idx_beta), log(sfun_values(idx_w2, idx_beta)), 100, 'filled')
     hold on
     surf(wq2, bq, log(sfun_values))
+    % aux = sprintf('%s) DOF %d', letters(i), i);
+    % title(aux)
+    view([18,10])
+    %plot3(wq2(idx_w2)*ones(1,1000), linspace(0,max(bq),1000), log(sfun_values(idx_w2, idx_beta))*ones(1,1000), 'r--', 'LineWidth', 2)
+    %plot3(linspace(0,max(wq2),1000), bq(idx_beta)*ones(1,1000), log(sfun_values(idx_w2, idx_beta))*ones(1,1000), 'k--', 'LineWidth', 2)
+
+    ax = gca;
+    ax.ZMinorTick = 'on';
+    ax.ZAxis.TickValues = linspace(log(min(min(sfun_values))), log(max(max(sfun_values))), log(200));
+
     xlabel('$\omega_{eq}^2(t)$')
     ylabel('$\beta_{eq}(t)$')
-    zlabel('$\Delta$Energy')
+    zlabel('$log(\Delta\sigma^2)$')
     colormap jet
     shading interp
-    %view([0,90])
 end
 
-saveas(fig, strcat('plots/3ddeltasigma_', str, '.pdf'))
+set(fig,'papersize',[10.5 5.5], 'position', [0 0 800 400]);
+print(fig,'plots/3ddeltasigma','-dpdf')
 
 %%
 fig = figure(2);
-for i=1:ndof
+for i=1:1
     sfun_values = data(i).sfun_values;
     bq = data(i).bq;
-    subplot(ndof,1,i)
+    subplot(1,1,i)
     hold on
-    plot(bq,log(sfun_values(data(i).idx_w2, :)))
-    scatter(bq(data(i).idx_beta),log(sfun_values(data(i).idx_w2, data(i).idx_beta)),30,'r','filled');
+    plot(bq,(sfun_values(data(i).idx_w2, :)))
+    scatter(bq(data(i).idx_beta),(sfun_values(data(i).idx_w2, data(i).idx_beta)),50,'r','filled');
+    set(gca, 'YScale', 'log')
+    % aux = sprintf('%s) DOF %d', letters(i), i);
+    % title(aux)
+    xlabel('$\beta_{eq}(t)$')
+    ylabel('$log(\Delta\sigma^2)$')
+    xlim([0, max(bq)])
+    grid
 end
 
-saveas(fig, strcat('plots/2ddeltasigma_', str, '.pdf'))
+set(fig,'papersize',[9.5 4.3], 'position', [0 0 750 300]);
+print(fig,'plots/2ddeltasigma','-dpdf')
