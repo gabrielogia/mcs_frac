74abb0b5db98d336bf4c03f3098b431272361db3
Gabriel Duarte <dacos033@umn.edu>
2025-02-24 14:42:46 -0600

    removing unnecessary comments

--- plot_survival_without_mcs.m
+++ plot_survival_without_mcs.m
@@ -14,7 +14,6 @@ end
 set(groot,'defaultAxesFontSize', 16)
 
 %%
-
 % Power spectrum (wn, eps)
 power_spectrum = "eps";
 
@@ -52,22 +51,15 @@ xy=y0_bw;
 % Barrier:
 lam = 0.25;
 
-% Time increment for the Monte Carlo simulation.
-dT = 1e-3;
-
-% Number of samples in the MCS:
-ns = 14000;
-
 %% plot survival for different q and lambda
-col = ["a" "b" "c" "d" "e" "f" "g" "h" "i"];
-
+col = ["a", "b", "c"];
 Q = [0.5 0.75];
 numQ = numel(Q);
 
 for jj=1:numQ
     q = Q(jj);
     str = sprintf('oscillator_%s_ndof_%d_fractional_%.2f_dt_%.4f_mcssamples_%d_damping_%.2f_stiffness_%.2f_barrier_%.2f_powerspectrum_%s_S0_%.2f', ...
-        oscillator, ndof, q, dT, ns, max(damping), max(stiffness), 0.25, power_spectrum, S0);
+        oscillator, ndof, q, 1e-3, 14000, max(damping), max(stiffness), 0.25, power_spectrum, S0);
     
     if (oscillator == "bw")
         str = strcat(str, sprintf('_bwparameters_a_%.2f_A_%.2f_beta_%.2f_gamma_%.2f_xy_%.2f', max(a_bw), A_bw, beta_bw, gamma_bw, xy));
@@ -75,71 +67,41 @@ for jj=1:numQ
         str = strcat(str, sprintf('_duffingparameter_epx_%.2f', max(epx)));
     end
     
-    % load mcs previous results
-    % if exist(strcat('data/mcs/mcs_', str, '.mat'))
-    %     load(strcat('data/mcs/mcs_', str, '.mat'))
-    %     run_mcs = false;
-    % else
-    %     run_mcs = true;
-    % end
-    
     load(strcat('data/firsttimepassage_', str, '.mat'))
     load(strcat('data/omegaeq_', str, '.mat'))
     load(strcat('data/betaeq_', str, '.mat'))
     
     lam = [0.25, 0.50, 0.75];
     numB = numel(lam);
-    h = gobjects(numel(lam),ndof);
 
     for ii=1:numB
         count = 1;
         B = sqrt(smaxi)*lam(ii);
-        tf = zeros(ns,ndof);
-
-        % for i=1:ns
-        %     for j=1:ndof
-        %         amp=amplitude(j,:,i);
-        %         time_aux = time_out(abs(amp) > B);
-        % 
-        %         if numel(time_aux)==0
-        %             tf(i,j) = NaN;
-        %         else
-        %             tf(i,j) = time_aux(1);
-        %         end
-        %     end
-        % end
-
         P = survival_probability(B*ones(ndof), c, time, numel(time), beta_eq, omega_eq_2, 15, S0);
     
         fig = figure(1);
         for i=1:ndof
-            % fpt = tf(:,i);
-            % fpt = fpt(fpt>0);
-            % [fpp,tfp]=ksdensity(fpt,'width',0.1,'Function','survivor');
             subplot(1,ndof+1,i);
             hold on
             if (lam(ii) == 0.25)
                 if (q == 0.75)
-                    h(:,i) = plot(time, P(i,:)','k','linewidth',2);
+                    plot(time, P(i,:)','k','linewidth',2);
                 else
-                    h(:,i) = plot(time, P(i,:)','--k','linewidth',2);
+                    plot(time, P(i,:)','--k','linewidth',2);
                 end
-                % plot(tfp, fpp,'--g','linewidth',2);
             elseif (lam(ii) == 0.50)
                 if (q == 0.75)
-                    h(:,i) = plot(time, P(i,:)','b','linewidth',2);
+                    plot(time, P(i,:)','b','linewidth',2);
                 else
-                    h(:,i) = plot(time, P(i,:)','--b','linewidth',2);
+                    plot(time, P(i,:)','--b','linewidth',2);
                 end
-                % plot(tfp, fpp,'--g','linewidth',2);
             else
                 if (q == 0.75)
-                    h(:,i) = plot(time, P(i,:)','r','linewidth',2);
+                    plot(time, P(i,:)','r','linewidth',2);
                 else
-                    h(:,i) = plot(time, P(i,:)','--r','linewidth',2);
+                    plot(time, P(i,:)','--r','linewidth',2);
                 end
             end
-            % fprintf("MSE: %.4f, q = %.2f, lambda = %.2f, dof: %d\n", mse(P(i,1:numel(fpp)),fpp), q, lam(ii), i)
             title(sprintf("%s) DOF: %d", col(i), i))
             xlabel('Time')
             ylabel('Survival propability')
