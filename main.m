ddc91332cc7848828d543c1882af560d5d47b1fa
Gabriel Duarte <dacos033@umn.edu>
2025-04-30 22:49:55 -0500

    adding new oscillator

--- main.m
+++ main.m
@@ -11,8 +11,8 @@ rng(1111);
 % Power spectrum (wn, eps)
 power_spectrum = "eps";
 
-% Oscillator ('bw', 'duffing')
-oscillator = "bw";
+% Oscillator ('bw', 'duffing', 'soft')
+oscillator = "soft";
 
 % Is Base motion / non-stationary (excitation):
 is_base = false;
@@ -25,10 +25,13 @@ ndof = 3;
 S0 = 0.2;
 
 % Fractional derivative:
-q = 0.999; 
+q = 0.9999; 
 
-% Nonlinearity parameter:
+% Duffing Nonlinearity parameter:
 epx = 1.0*ones(1,ndof);
+
+% Softening Nonlinearity parameter:
+alpha = 0.5*ones(1,ndof);
 
 % Mass, damping, and stiffness vectors: 
 mass = 1*ones(1,ndof); 
@@ -56,7 +59,7 @@ lam = 0.50;
 dT = 1e-3;
 
 % Construct matrices M, C, and K:
-if (oscillator == "duffing")
+if (oscillator == "duffing" || oscillator == "soft")
     [M, C, K] = get_mck(mass, damping, stiffness, ndof);
 elseif (oscillator == "bw")
     [M, C, K] = get_mck_bw(mass, damping, stiffness, a_bw, ndof, y0_bw);
@@ -66,7 +69,7 @@ end
 fmax_ps = 50;
 
 % Number of samples in the MCS:
-ns = 14000;
+ns = 3000;
 
 % Discretization in time and frequency for the Statistical Linearization:
 ntime = 400;
@@ -78,8 +81,10 @@ str = sprintf('oscillator_%s_ndof_%d_fractional_%.2f_dt_%.4f_mcssamples_%d_dampi
 
 if (oscillator == "bw")
     str = strcat(str, sprintf('_bwparameters_a_%.2f_A_%.2f_beta_%.2f_gamma_%.2f_xy_%.2f', max(a_bw), A_bw, beta_bw, gamma_bw, xy));
-else
+elseif (oscillator == "duffing")
     str = strcat(str, sprintf('_duffingparameter_epx_%.2f', max(epx)));
+else
+    str = strcat(str, sprintf('_softparameter_alpha_%.2f', max(alpha)));
 end
 
 % load mcs previous results
@@ -94,16 +99,18 @@ end
 disp(["Running Statistical Linearization:" oscillator])
 
 time = linspace(1e-3, T, ntime);
+omega_n = sqrt(eig(inv(M)*K));
+freq = linspace(0,fmax_ps,nfreq);
 
-if (oscillator == "duffing")
-    omega_n = sqrt(eig(inv(M)*K));
-    freq = linspace(0,fmax_ps,nfreq);
-    
+if (oscillator == "duffing")    
     [varx_sl, varv_sl, conv, k_eq, c_eq] = ...
     statistical_linearization(mass, damping, stiffness, M, C, K, freq, time, ndof, epx, q, is_base, S0);
 elseif (oscillator == "bw")
     [varx_sl, varv_sl, conv, k_eq, c_eq] = ...
     statistical_linearization_bw(M, C, K, time, A_bw, gamma_bw, beta_bw, fmax_ps, nfreq, q, xy, S0);
+elseif (oscillator == "soft")
+    [varx_sl, varv_sl, conv, k_eq, c_eq] = ...
+    statistical_linearization_soft(mass, damping, stiffness, M, C, K, freq, time, ndof, alpha, q, S0);
 end
 
 %% Equivalent damping and stiffness
@@ -113,7 +120,7 @@ disp("Getting omega and beta.");
 %% Get c(t) by solving the ODE from stochastic averaging.
 disp("Solving the ODE to find c(t):")
 
-ic = 0.00000001;
+ic = 0.000005;
 c = zeros(ndof, numel(time));
 
 for i=1:ndof
@@ -136,13 +143,17 @@ for i=1:ndof
 end
 
 %% Monte Carlo Simulation
-if run_mcs
+if 1
     disp('Running MCS:')
     
     tic
     if (oscillator == "duffing")
     [varx_mcs, time_out, first_passage_time,response,velocity,amplitude] = ...
         monte_carlo(ns,M,C,K,epx,q,mass,damping,stiffness,fmax_ps,...
+        nonstat, is_base,T,dT, barrier, S0);
+    elseif (oscillator == "soft")
+    [varx_mcs, time_out, first_passage_time,response,velocity,amplitude] = ...
+        monte_carlo_soft(ns,M,C,K,alpha,q,mass,damping,stiffness,fmax_ps,...
         nonstat, is_base,T,dT, barrier, S0);
     elseif (oscillator == "bw")
         [amplitude, time_out, first_passage_time] = ...
@@ -155,6 +166,14 @@ if run_mcs
 else
     disp('Jumping MCS')
 end
+
+%% plot variance
+plot(time, varx_sl)
+hold on;
+plot(time_out, varx_mcs)
+xlabel('Time')
+ylabel('Variance')
+legend('DOF 1 - SL', 'DOF 2 - SL', 'DOF 3 - SL','DOF 1 - MCS', 'DOF 2 - MCS', 'DOF 3 - MCS');
 
 %% First passage
 disp("Getting first passage")
@@ -242,7 +261,7 @@ save(strcat('data/betaeq_', str, '.mat'), "time", "beta_eq", "beta_original")
 %% Survival Probability
 bt = beta_eq;
 tic
-P = survival_probability(barrier, c, time, numel(time), bt, omega_eq_2, 15, S0);
+P = survival_probability(barrier, c, time, numel(time), bt, omega_eq_2, 15, S0, oscillator, q);
 toc
 
 fig = figure('color',[1 1 1]);
---
+++ monte_carlo_soft.m
@@ -0,0 +1,86 @@
+function [vx,time_out,first_passage_time,state,velo,amplitude] = ...
+    monte_carlo_soft(ns,M,C,K,alpha,q,mass,damping,stiffness,fmax_ps,nonstat,...
+    is_base, T, dT, bar, S0) 
+
+    Mi = inv(M);
+    MiC = Mi*C;
+    MiK = Mi*K;
+    ndof = size(M,1);
+    
+    for i=1:ndof
+        EPS{i}.fun = @(f,t)( evolutionary_power_spectrum(f, t, S0) );
+    end
+    
+    tt = 0:dT:T;
+    nt = numel(tt);
+    
+    Nrk = 3000;
+    state = zeros(ndof,Nrk,ns);
+    velo = zeros(ndof,Nrk,ns);
+    wexc = zeros(ndof,nt,ns);
+    amplitude = zeros(ndof,Nrk,ns);
+    time_out = linspace(tt(1), tt(end), Nrk)';
+    
+    parfor i=1:ns
+        wv = simulate_process(tt, ndof, EPS, nonstat, is_base, fmax_ps);
+        xini = zeros(3*ndof,1);
+
+        [t1,X] = fde45_mdof_new(@(t,x,w) fun_fde_mdof_soft(t,x,ndof,MiC,MiK,Mi,...
+            mass,damping,stiffness,alpha,is_base,w), tt, xini, q, wv);
+
+        for j=1:ndof
+            xint = interp1(t1,X(j, :),time_out,'pchip'); %just the displacement
+            vint = interp1(t1,X(j+ndof, :),time_out,'pchip'); %just the displacement
+            state(j, :, i) = xint;
+            velo(j, :, i) = vint;
+            amplitude(j,:,i) = get_amplitude(xint);
+        end
+    end
+
+    [~, n_time, ~] = size(state); % z time dimension.
+
+    for i=1:n_time
+        for j=1:ndof
+            vx(i, j) = var(state(j, i, :));
+        end
+    end
+
+    vx = vx';
+
+    %% First passage time
+    first_passage_time = zeros(ns,ndof);
+    for i=1:ns
+        
+        for j=1:ndof
+            barrier = bar(j);
+            sample_path = amplitude(j,:,i);
+            
+            time_aux = time_out(abs(sample_path) > barrier);
+
+            if numel(time_aux)==0
+                first_passage_time(i,j) = 0;
+                warning('Check the barrier');
+            else
+                first_passage_time(i,j) = time_aux(1);
+            end
+        end
+    end
+end
+
+function amp = get_amplitude(x)
+    [r,c] = size(x);
+
+    if r>c
+        x = x';
+    end
+
+    npadd = ceil(max(r,c)/5);
+    X = padarray(x',npadd,'symmetric')';
+
+    if r>c
+        X = X';
+    end
+  
+    A = abs(hilbert(X));
+    amp = A(npadd+1:end-npadd);
+end
